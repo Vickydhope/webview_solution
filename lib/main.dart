cd43293fb1affb0d0d4e677e4c3a874740d84632
vicky_dhope <vicky.dhope@netsurfnetwork.com>
2023-01-31 12:10:43 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const LandingScreen(),
+    );
+  }
+}
+
+class LandingScreen extends StatelessWidget {
+  const LandingScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          child: const Text("Open Webview"),
+          onPressed: () {
+            Navigator.of(context).push(MaterialPageRoute(
+              builder: (context) => const WebViewApp(),
+            ));
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class WebViewApp extends StatefulWidget {
+  const WebViewApp({super.key});
+
+  @override
+  State<WebViewApp> createState() => _WebViewAppState();
+}
+
+class _WebViewAppState extends State<WebViewApp> {
+  final WebViewController controller = WebViewController();
+  var loadingPercentage = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    controller.clearLocalStorage();
+    controller
+      ..loadRequest(Uri.parse("https://effulgent-jalebi-66cec7.netlify.app"))
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setNavigationDelegate(NavigationDelegate(
+        onPageStarted: (url) {
+          setState(() {
+            loadingPercentage = 0;
+
+          });
+        },
+        onProgress: (progress) {
+          setState(() {
+            loadingPercentage = progress;
+          });
+        },
+        onPageFinished: (url) {
+          setState(() {
+            loadingPercentage = 100;
+          });
+        },
+      ))..addJavaScriptChannel(
+      "messageHandler",
+      onMessageReceived: (message) {
+        if (message.message == "Close") Navigator.of(context).pop();
+        ScaffoldMessenger.of(context)
+            .showSnackBar(SnackBar(content: Text(message.message)));
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter WebView'),
+      ),
+      body: Column(
+        children: [
+          if (loadingPercentage < 100)
+            LinearProgressIndicator(
+              value: loadingPercentage / 100.0,
+            ),
+          Expanded(
+            child: WebViewWidget(
+              controller: controller,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
